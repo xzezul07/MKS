5c6d36aeeef7236883e7637b33f70d68e3572e9f
Eliska Zezulova <xzezul07@vutbr.cz>
2021-11-04 10:55:08 +0100

    ghfs

---
+++ cv_06/Core/Inc/sct.h
@@ -0,0 +1,15 @@
+/*
+ * sct.h
+ *
+ *  Created on: 7. 10. 2021
+ *      Author: student
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value, uint8_t led);
+
+#endif /* SCT_H_ */
